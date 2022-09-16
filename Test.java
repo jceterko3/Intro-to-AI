50b10268bacd5ed5c3aa4f50250f166ebf209620
ajk267 <ajk267@rutgers.edu>
2022-09-15 21:51:47 -0400

    optimized reading file
    eliminated 0,0 rows in bkd by reading to arraylist of arraylists and then converting to int[][] instead of predefined int[][]

--- Test.java
+++ Test.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 
 import java.util.Scanner;
 import java.io.*;
+import java.util.ArrayList;
+
 
 public class Test {
 
@@ -11,12 +13,19 @@ public class Test {
         String fileName = args[0];
 
         // initializing variables used to create grid & start/goal vertex
-        int sx = 0; int sy = 0; int gx = 0; int gy = 0; int col = 0; int row = 0; 
+        int sx = 0; //start x
+        int sy = 0; //start y
+        int gx = 0; //goal x
+        int gy = 0; //goal y
+        int col = 0; //#columns
+        int row = 0; //#rows
         
         // array that holds all blocked cells 
-        int bkd[][] = new int[100][50];                                     // this is hard coded might need to change 
-        int isBlocked = 3; int count = 0;
+        int isBlocked; 
+        int count = 0;
 
+        ArrayList<ArrayList<Integer>> bkd = new ArrayList<ArrayList<Integer>>();
+        
         try{
             Scanner scan = new Scanner(new File(fileName));
 
@@ -32,23 +41,26 @@ public class Test {
                 col = scan.nextInt();
                 row = scan.nextInt();
 
+
+
+
+                
                 // obtaining the cells, not sure if this has to be optimized in some way 
-                // he mentioned in the assignment we can only do one initialization for vertices?       
-                int pointer1 = 0; int pointer2 = 0;     
-                for(int tracker1 = 0; tracker1 < col*row; tracker1++){
+                // he mentioned in the assignment we can only do one initialization for vertices? 
+                for(int i = 0; i < col*row; i++){
+                    //hold all variables
+                    int r = scan.nextInt();
+                    int c = scan.nextInt();
+                    isBlocked = scan.nextInt(); 
 
-                    bkd[pointer1][pointer2] = scan.nextInt();                     
-                    bkd[pointer1][pointer2+1] = scan.nextInt();  
-                    isBlocked = scan.nextInt();
-
-                    if(isBlocked == 0){              
-                        bkd[pointer1][pointer2] = 0;                     
-                        bkd[pointer1][pointer2+1] = 0;  
-                    }
-                    else{           
-                        pointer1++;
+                    if(isBlocked == 1){    //if blocked then add to arraylist
+                        bkd.add(new ArrayList<Integer>());
+                        bkd.get(count).add(r);   
+                        bkd.get(count).add(c);  
+                        System.out.println(bkd.get(count).toString());
                         count++;
                     }
+
                 }
 
             }
@@ -60,14 +72,28 @@ public class Test {
 	    }
 
 
-        Frame grid = new Frame(sx,sy,gx,gy,col,row,bkd,count);
-
         // maybe here ask about viewing a vertex and displaying info in terminal?
         /*Scanner question = new Scanner(System.in);
         System.out.println("Enter a vertex (x,y) to view more info");
                 
         String answer = question.nextLine();
         System.out.println("VALUES : ");*/
+
+        int[][] b = bkd.stream().map(x -> x.stream().mapToInt(Integer::intValue).toArray()).toArray(int[][]::new); 
+
+        //test to print array of blocked cells (delete later)
+        for(int i=0; i<b.length; i++){
+            System.out.print(b[i][0]+",");
+            System.out.println(""+b[i][1]);
+        }
+
+
+        Frame grid = new Frame(sx,sy,gx,gy,col,row,b,count);
+
+         
+         
+   
+
 
     }
     
